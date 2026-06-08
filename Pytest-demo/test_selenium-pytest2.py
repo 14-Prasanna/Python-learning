4459c13461aa7a2051252743e08db97a43ff9a3f
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2026-06-08 23:02:44 +0530

    Update test_selenium-pytest2.py

--- Pytest-demo/test_selenium-pytest2.py
+++ Pytest-demo/test_selenium-pytest2.py
@@ -4,25 +4,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
 import time
-
+import os
 
 @pytest.mark.parametrize("browser", ["firefox", "edge"])
 @pytest.mark.parametrize("search_term", ["selenium", "pytest", "selenium locators"])
 def test_google_search(browser, search_term):
-    
     driver = None
-    
-    if browser == "firefox":
-        options = FirefoxOptions()
-        options.add_argument("--headless")
-        driver = webdriver.Firefox(options=options)
-        
-    elif browser == "edge":
-        options = EdgeOptions()
-        options.add_argument("--headless")
-        driver = webdriver.Edge(options=options)
-    
     try:
+        if browser == "firefox":
+            options = FirefoxOptions()
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            driver = webdriver.Firefox(options=options)
+            
+        elif browser == "edge":
+            options = EdgeOptions()
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            driver = webdriver.Edge(options=options)
+        
         driver.maximize_window()
         driver.get("https://www.google.com")
         
@@ -32,7 +34,7 @@ def test_google_search(browser, search_term):
         driver.find_element(By.NAME, "btnK").click()
         time.sleep(5)
         
-        print(f" Search completed for: '{search_term}' on {browser.upper()}")
+        print(f"Search completed for: '{search_term}' on {browser.upper()}")
         
     finally:
         if driver:
