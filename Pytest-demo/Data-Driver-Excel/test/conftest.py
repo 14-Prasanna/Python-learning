5a60b1f9f86d132732c980cd78630b4c3d064b83
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2026-06-09 22:43:58 +0530

    Update conftest.py

--- Pytest-demo/Data-Driver-Excel/test/conftest.py
+++ Pytest-demo/Data-Driver-Excel/test/conftest.py
@@ -1,12 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture()
 def setup():
-    driver = webdriver.Firefox()
+    options = FirefoxOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Firefox(options=options)
     driver.maximize_window()
     driver.get("https://demoblaze.com/")
     wait = WebDriverWait(driver, 10)
