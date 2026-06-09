dfd81aa1f92428dfbd64bb72e3e506fa2cc96ce7
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2026-06-09 22:28:46 +0530

    Update conftest.py

--- Pytest-demo/Demo-blaze/conftest.py
+++ Pytest-demo/Demo-blaze/conftest.py
@@ -1,19 +1,45 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from resources.ConfigReader import get_value
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="firefox",
+        help="Browser to run tests: firefox or edge"
+    )
 
 
 @pytest.fixture()
 def setup(request):
 
-    url = get_value("Basic Info", "url")
-    browser = get_value("Basic Info", "browser")
+    url     = get_value("Basic Info", "url")
+    browser = request.config.getoption("--browser")
 
     if browser.lower() == "firefox":
-        driver = webdriver.Firefox()
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+        driver = webdriver.Firefox(options=options)
+
+    elif browser.lower() == "edge":
+        options = EdgeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920,1080")
+        driver = webdriver.Edge(options=options)
+
     else:
-        raise Exception("Invalid browser")
+        raise Exception(f"Unsupported browser: {browser}")
 
     driver.get(url)
     driver.maximize_window()
@@ -21,7 +47,7 @@ def setup(request):
     wait = WebDriverWait(driver, 10)
 
     request.cls.driver = driver
-    request.cls.wait = wait
+    request.cls.wait   = wait
 
     yield
 
